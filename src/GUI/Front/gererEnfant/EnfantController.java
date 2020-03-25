308ea6df840ad978a062eef9720c1d136ee5e863
wifeko <wifek.ouerghemmi@esprit.tn>
2020-03-25 13:32:34 +0100

    GuiFrontWifekCommit2

--- src/GUI/Front/gererEnfant/EnfantController.java
+++ src/GUI/Front/gererEnfant/EnfantController.java
@@ -5,24 +5,44 @@
  */
 package GUI.Front.gererEnfant;
 
+import Entity.wifek.Bus;
+import Entity.wifek.enfant;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import javax.swing.JOptionPane;
+import org.controlsfx.control.Notifications;
+import service.wifek.CrudBusService;
 import service.wifek.CrudEnfantService;
 
 /**
@@ -31,6 +51,9 @@ import service.wifek.CrudEnfantService;
  * @author Imen
  */
 public class EnfantController implements Initializable {
+    public ArrayList<enfant> ran;
+    CrudEnfantService buss=new CrudEnfantService();
+    
     @FXML
     private TabPane tabpane;
     @FXML
@@ -42,33 +65,70 @@ public class EnfantController implements Initializable {
     @FXML
     private Button retour4;
     @FXML
-    private TableView<?> tabAffiche;
+    private TableView<enfant> tabAffiche;
     @FXML
-    private TableColumn<?, ?> colid;
+    private TableColumn<enfant, Integer> colid;
     @FXML
-    private TableColumn<?, ?> colsexe;
+    private TableColumn<enfant, String> colsexe;
     @FXML
-    private TableColumn<?, ?> colnom;
+    private TableColumn<enfant, String> colnom;
     @FXML
-    private TableColumn<?, ?> colpre;
+    private TableColumn<enfant, String> colpre;
     @FXML
-    private TableColumn<?, ?> colage;
+    private TableColumn<enfant, Integer> colage;
     @FXML
-    private TableColumn<?, ?> colidbus;
+    private TableColumn<enfant, Integer> colidbus;
     @FXML
-    private TableColumn<?, ?> colidp;
-//hethi khedmtik linaa
+    private TableView<enfant> tabAffiche1;
+    @FXML
+    private TableColumn<?,?> colid1;
+    @FXML
+    private TableColumn<enfant, String> colsexe1;
+    @FXML
+    private TableColumn<enfant, String> colnom1;
+    @FXML
+    private TableColumn<enfant, String> colpre1;
+    @FXML
+    private TableColumn<enfant, Integer> colage1;
+    @FXML
+    private TableColumn<enfant, Integer> colidbus1;
+    @FXML
+    private Button buttonAdd;
+    @FXML
+    private Button updateButton;
+    @FXML
+    private Button suppButton;
+    @FXML
+    private TextField nom;
+    @FXML
+    private TextField prenom;
+    @FXML
+    private TextField trajet;
+    @FXML
+    private ComboBox<String> sexecombo;
+    @FXML
+    private TextField age;
+
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO               
-        afficher();
+       afficher1();
+        afficher2();
+        //combo sexe
+        ObservableList<String> sexe=FXCollections.observableArrayList("Garcon","Fille");
+        sexecombo.setValue("sexe");
+       sexecombo.setItems(sexe);
+       //combo age
+       
+       
 
     }    
     
-            private void afficher() {
+           private void afficher1() {
     CrudEnfantService sp = new CrudEnfantService();
       List buss=sp.afficherEnfant();
        ObservableList et=FXCollections.observableArrayList(buss);
@@ -80,9 +140,20 @@ public class EnfantController implements Initializable {
         colpre.setCellValueFactory(new PropertyValueFactory<>("prenom")); 
         colage.setCellValueFactory(new PropertyValueFactory<>("age")); 
         colidbus.setCellValueFactory(new PropertyValueFactory<>("id_Bus"));
-        colidp.setCellValueFactory(new PropertyValueFactory<>("idParent"));
 
-
+}
+      private void afficher2() {
+    CrudEnfantService sp = new CrudEnfantService();
+      List buss=sp.afficherEnfant();
+       ObservableList et=FXCollections.observableArrayList(buss);
+       tabAffiche1.setItems(et);
+     ObservableList observableList = FXCollections.observableArrayList(buss);
+        colid1.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colsexe1.setCellValueFactory(new PropertyValueFactory<>("sexe"));
+        colnom1.setCellValueFactory(new PropertyValueFactory<>("nom"));
+        colpre1.setCellValueFactory(new PropertyValueFactory<>("prenom")); 
+        colage1.setCellValueFactory(new PropertyValueFactory<>("age")); 
+       colidbus1.setCellValueFactory(new PropertyValueFactory<>("id_Bus"));
 }
 
     @FXML
@@ -135,5 +206,131 @@ public class EnfantController implements Initializable {
     stage.close();
      
     }
+
+    @FXML
+    private void ajoutB(ActionEvent event)  throws SQLException {
+
+        CrudEnfantService b= new CrudEnfantService();
+    if( sexecombo.getValue().isEmpty() || nom.getText().isEmpty() || prenom.getText().isEmpty() || age.getText().isEmpty() || trajet.getText().isEmpty() ){
+    Alert alertt=new Alert(Alert.AlertType.ERROR);
+    alertt.setTitle("WARNING!");
+    alertt.setHeaderText(null);
+    alertt.setContentText("Merci de vérifier que vous avez remplit tout les champs");
+    alertt.showAndWait();
+    }
+    else if (!age.getText().matches("[3-6]")){
+          Alert alert1 = new Alert(Alert.AlertType.ERROR);
+        alert1.setTitle("Valider Age");
+            alert1.setHeaderText(null);
+            alert1.setContentText("L'âge doit être entre 3 ans et 6 ans");
+            alert1.showAndWait();
     
+    }else if(!nom.getText().matches("^[a-zA-Z\\s]*$")){
+            Alert alert11 = new Alert(Alert.AlertType.ERROR);
+            alert11.setTitle("Verifier");
+            alert11.setHeaderText(null);
+            alert11.setContentText("Le champ NOM accepte que les lettres ");
+            alert11.showAndWait();
+         }else if( !prenom.getText().matches("^[a-zA-Z\\s]*$")){
+         Alert alert1 = new Alert(Alert.AlertType.ERROR);
+            alert1.setTitle("Verifier");
+            alert1.setHeaderText(null);
+            alert1.setContentText("Le champ PRENOM accepte que les lettres ");
+            alert1.showAndWait();
+         
+         }else{ 
+        enfant bus=new enfant(sexecombo.getValue(),
+            nom.getText(),
+            prenom.getText(),
+            Integer.parseInt(age.getText()),
+            Integer.parseInt(trajet.getText())
+    );   
+    b.ajouterEnfant(bus);
+        
+    
+    Notifications notif=Notifications.create()
+            .title("Bus ajouté")
+            .text("Un nouveau Bus vient d'être ajoutée !")
+            .darkStyle().graphic(null).hideAfter(Duration.seconds(5))
+            .position(Pos.TOP_LEFT)
+            .onAction(new EventHandler<ActionEvent>() {
+                    public void handle(ActionEvent event) {
+                        System.out.println("Clicked ont notif");
+                    }
+                });
+    notif.showConfirm();
+    }
+    refreshB(event);
+  // clearEnfant(event);
+    }
+
+    @FXML
+    private void modifB(ActionEvent event) throws SQLException{
+       // Bus cc = (Bus)tabAffiche.getSelectionModel().getSelectedItem();
+    CrudEnfantService cs = new CrudEnfantService();
+      enfant b=tabAffiche1.getSelectionModel().getSelectedItem();
+      cs.modifierEnfant(sexecombo.getValue(),nom.getText(),prenom.getText(),Integer.parseInt(age.getText()),Integer.parseInt(trajet.getText()),b.getId());
+      refreshB(event);
+      clearEnfant(event);
+    }
+      /*  Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Notification");
+        alert.setContentText("Etes vous sure de vouloir supprimer ?");
+        Optional<ButtonType> result = alert.showAndWait();
+        
+        ButtonType button = result.orElse(ButtonType.CANCEL);
+        
+        CrudBusService rs = new CrudBusService();
+         Bus cc = (Bus)tabAffiche.getSelectionModel().getSelectedItem();
+        System.out.println(cc);
+        if(button == ButtonType.OK){*/
+    @FXML
+    private void supprimeB(ActionEvent event) throws SQLException {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Notification");
+        alert.setContentText("Etes vous sure de vouloir supprimer ?");
+        Optional<ButtonType> result = alert.showAndWait();
+        ButtonType button = result.orElse(ButtonType.CANCEL);
+        
+        CrudEnfantService rs = new CrudEnfantService();
+         enfant cc = (enfant)tabAffiche1.getSelectionModel().getSelectedItem();
+        System.out.println(cc);
+        if(button == ButtonType.OK){
+            JOptionPane.showMessageDialog(null, "choisir Enfant");
+        }else{
+            rs.supprimerEnfant(cc.getId());
+            refreshB(event);
+            clearEnfant(event);
+           JOptionPane.showMessageDialog(null, "Enfant supprimer");
+         
+        cc=null;
+    }
+    }
+    
+
+    @FXML
+    private void modifAff(MouseEvent event) {
+    enfant b=tabAffiche1.getSelectionModel().getSelectedItem();
+    sexecombo.setValue(b.getSexe());
+    nom.setText(b.getNom());
+    prenom.setText(b.getPrenom());
+    age.setText(String.valueOf(b.getAge()));
+    trajet.setText(String.valueOf(b.getId_Bus()));
+    
+    }
+    
+    private void refreshB(ActionEvent event) throws SQLException{
+        List<enfant> listB=new ArrayList<>();
+        CrudEnfantService   cr = new CrudEnfantService();
+        listB = cr.afficherEnfant();
+        ObservableList <enfant> data = FXCollections.observableArrayList(listB);
+        tabAffiche1.setItems(data);
+    }
+    
+    private void clearEnfant(ActionEvent event) {
+    nom.clear();
+    prenom.clear();   
+    age.clear();
+    trajet.clear();
+    }
 }
